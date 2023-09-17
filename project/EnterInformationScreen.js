0f65572c74d7deacca0f9d8b71b8d74a79e63e97
Loldude0 <aryan.tajne@gmail.com>
2023-09-16 23:34:02 -0400

    vars

--- project/EnterInformationScreen.js
+++ project/EnterInformationScreen.js
@@ -67,6 +67,8 @@ function EnterInformationScreen({ navigation, route }) {
             if (response.ok) {
                 const responseData = await response.text();
                 console.log('server response:', responseData);
+                userId = responseData.split("-")[0]
+                nurseId = responseData.split("-")[1]
                 navigation.navigate('Home', { userInfo: responseData })
             } else {
                 console.error('server error:', response.status, response.statusText);
